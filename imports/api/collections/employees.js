0b1490c0cc6914ab53c0e2f8595722559d640cee
pavlovanton <panton.anton98@gmail.com>
2018-03-28 19:33:54 +0300

    Databases will connect with promises now

--- imports/api/collections/employees.js
+++ imports/api/collections/employees.js
@@ -4,23 +4,24 @@ import { Mongo } from 'meteor/mongo';
 export const Employees = new Mongo.Collection('employees');
 
 export const subscribeEmployees = (cb) => {
-  let attempt = 1
-
-  console.group('EMPLOYEES_DB_CONNECTION');
-
-  const dbConnection = setInterval(() => {
-    if (!Employees.find().fetch().length) {
-      Meteor.subscribe('employees');
-      console.log(`Connection DB. Attempt: ${attempt}`);
-      attempt += 1;
-    } else {
-      console.log(`DB was successfully connected.\n`);
-      console.info('DB state', Employees.find().fetch());
-      console.groupEnd('EMPLOYEES_DB_CONNECTION');
-      clearInterval(dbConnection);
-      cb();
-    }
-  }, 500);
+  return new Promise((resolve, reject) => {
+    let attempt = 1
+    console.group('EMPLOYEES_DB_CONNECTION');
+    const dbConnection = setInterval(() => {
+      if (!Employees.find().fetch().length) {
+        Meteor.subscribe('employees');
+        console.log(`Connection DB. Attempt: ${attempt}`);
+        attempt += 1;
+      } else {
+        console.log(`DB was successfully connected.\n`);
+        console.info('DB state', Employees.find().fetch());
+        console.groupEnd('EMPLOYEES_DB_CONNECTION');
+        clearInterval(dbConnection);
+        cb();
+        resolve();
+      }
+    }, 500);
+  });
 }
 
 
